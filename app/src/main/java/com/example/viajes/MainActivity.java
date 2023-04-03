c2bea603f8e97f2adfa59c015e77b76cadd9b805
isabl101 <mudemox1@gmail.com>
2023-04-02 19:33:35 -0500

    se crea segundo layout

--- app/src/main/java/com/example/viajes/MainActivity.java
+++ app/src/main/java/com/example/viajes/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.viajes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSiguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void irASegundaPag (View view){
+        Intent i = new Intent(this, layout2.class);
+        startActivity(i);
+    }
+
 }
