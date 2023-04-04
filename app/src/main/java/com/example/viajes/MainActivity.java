534d1a4e7da47aca6b0eb9658c1b692b87259bce
isabl101 <mudemox1@gmail.com>
2023-04-03 22:18:34 -0500

    se crea el tercer layout

--- app/src/main/java/com/example/viajes/MainActivity.java
+++ app/src/main/java/com/example/viajes/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
     }
     public void irASegundaPag (View view){
         Intent i = new Intent(this, layout2.class);
--- app/src/main/java/com/example/viajes/layout3.java
+++ app/src/main/java/com/example/viajes/layout3.java
@@ -6,17 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class layout2 extends AppCompatActivity {
+public class layout3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_layout2);
+        setContentView(R.layout.activity_layout3);
 
         getSupportActionBar().hide();
     }
-    public void irATerceraPag (View view){
-        Intent i = new Intent(this, layout3.class);
+
+    /*public void irACuartaPag (View view){
+        Intent i = new Intent(this, layout4.class);
         startActivity(i);
-    }
+    }*/
 }
