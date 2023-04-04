e4975ddb93a5e71fc1a057649fb5de4ff264d4f8
isabl101 <mudemox1@gmail.com>
2023-04-04 13:05:50 -0500

    se termina uarto layout

--- app/src/main/java/com/example/viajes/layout4.java
+++ app/src/main/java/com/example/viajes/layout4.java
@@ -6,18 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class layout3 extends AppCompatActivity {
+public class layout4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_layout3);
+        setContentView(R.layout.activity_layout4);
 
         getSupportActionBar().hide();
     }
 
-    public void irACuartaPag (View view){
-        Intent i = new Intent(this, layout4.class);
-        startActivity(i);
-    }
 }
